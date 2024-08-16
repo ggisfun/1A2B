15d78ef31d333c7d545fb4ae8ff286d3c03e39a0
Adam <bgisgone@gmail.com>
2024-08-16 23:39:46 +0800

    Initial Commit

---
+++ 1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B
+//
+//  Created by Adam Chen on 2024/8/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
